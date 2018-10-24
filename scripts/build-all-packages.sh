23682373cb026b58affca85379d2afeb6cd2aa08
Victoria Offord <vo1@sanger.ac.uk>
2018-10-24 15:01:06 +0100

    Modify script name to build-all-packages

---
+++ scripts/build-all-packages.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]
+then
+	echo "Usage: build-packages.sh <recipes_directory>"
+	echo "Incorrect number of arguments."
+        exit 1;
+fi
+
+top_level_dir=`pwd`
+
+for package in recipes/*/
+do
+	echo "Found package: "$package
+
+	package_dir=$top_level_dir"/"$package
+	cd $package_dir
+
+	num_sub_dirs=`ls -l | grep -c ^d`
+	echo "Versions found: "$num_sub_dirs
+
+	if [ $num_sub_dirs -gt 0 ]
+	then
+		for version in */
+		do
+			echo "Found version: "$version" in "$package
+			cd $version
+			version_dir=`pwd`
+			build-package.sh $version_dir
+			cd $package_dir
+		done
+	else
+		build-package.sh $package_dir
+	fi
+
+	cd $top_level_directory
+done
