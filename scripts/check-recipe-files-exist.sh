576d2ca144af0f6efb1b980d7668a3b3e7a3e2f1
Victoria Offord <vo1@sanger.ac.uk>
2018-10-24 13:48:49 +0100

    Add script to validate recipes

---
+++ scripts/check-recipe-files-exist.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]
+then
+	echo "Usage: check-recipe-files-exist.sh <recipe_directory>"
+	echo "Incorrect number of arguments."
+	exit 1;
+fi
+
+recipe_dir=$1
+expected_files=(meta.yml build.sh)
+num_expected_files=${#expected_files[@]}
+found=0
+
+# Check whether expected recipe files are present
+for fname in "${expected_files[@]}"
+do
+	fpath=$recipe_dir"/"$fname
+        if [ ! -f "$fpath" ]
+	then
+               	echo "Recipe file not found: "$fpath
+	else
+       		(( found++ ))
+        fi
+done
+
+# Check number of files found matches expected number of files
+if [ $found -ne $num_expected_files ]
+then
+	echo "Number of expected recipe files ("$num_expected_files") does not match number found ("$found")"
+	exit 1;
+fi
