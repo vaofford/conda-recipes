02ef2448c8604e936d3defde541b6c04d5e383cf
Victoria Offord <vo1@sanger.ac.uk>
2018-10-24 14:59:21 +0100

    Create build-package.sh
    Script to build a single package

---
+++ scripts/build-package.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ $# -ne 1]
+then
+  echo "Usage: build-package.sh <package_directory>"
+  echo "Incorrect number of arguments."
+        exit 1;
+fi
+
+package_directory=$1
+
+"Checking package directory exists: "$package_directory
+if [ ! -d $package_directory ]
+then
+      echo "Package directory doesn't exist: "$package_directory
+      exit 1;  
+fi
+
+echo "Checking recipe files exist in package directory: "$package_directory
+check_recipe_cmd="check-recipe-files-exist.sh "$package_directory
+result=`$check_recipe_cmd`
+if [ ! -z "$result" ]
+then
+      echo "$result"
+      exit 1;
+fi
+
+echo "Building package: "$package_directory
+conda build $package_directory
